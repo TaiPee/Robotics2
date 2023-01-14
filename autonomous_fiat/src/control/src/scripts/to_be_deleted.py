a61d086fbe6a4b8385ee9a37fbaad5a866b16b80
ritapalmaa <ritapalma2000@gmail.com>
2023-01-14 20:48:39 +0100

    look_ahead_point

--- autonomous_fiat/src/control/src/scripts/to_be_deleted.py
+++ autonomous_fiat/src/control/src/scripts/to_be_deleted.py
@@ -3,22 +3,22 @@ import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def read_matrix_from_yaml(filepath):
     with open(filepath, 'r') as file:
         matrix = yaml.safe_load(file)
     return matrix
 
+def get_reference():
+    points = read_matrix_from_yaml("/home/rita/Robotics/Robotics2/autonomous_fiat/src/control/src/maps/reference_path.yaml")
+    points = np.array([v for v in points['reference_path']])
+
+    return points
+
 def plot_ref_path(points):
     plt.plot(points[:,0], points[:,1])
     plt.show()
 
-
-def closest_point():
-
-    points = read_matrix_from_yaml("/home/rita/Robotics/Robotics2/autonomous_fiat/src/control/src/maps/reference_path.yaml")
-    points = np.array([v for v in points['reference_path']])
-    atual_position = np.array([4,1])
+def closest_point(atual_position,points):
 
     closest_index = 0
     closest_distance = float("inf")
@@ -32,6 +32,27 @@ def closest_point():
 
     return closest_index
 
+def look_ahead_point(points, index, ref_distance):
+    
+    walking_path = 0
+    final_index = len(points)-1
+    pos = points[index]
+    for i, point in enumerate(points[index+1:-1]):
+        distance = np.sqrt(np.power(point[0] - pos[0],2) + np.power(point[1] - pos[1],2))
+        walking_path = walking_path + distance
+        if walking_path > ref_distance:
+            final_index = i + index+1
+            return final_index
+
+    return final_index
+
+
 
 if __name__ == '__main__':
-    closest_point()
+    reference_path = get_reference()
+    actual_position = np.array([4,1])
+    distance = 2
+    index =  closest_point(actual_position)
+    final_index = look_ahead_point(reference_path, index, distance)
+
+

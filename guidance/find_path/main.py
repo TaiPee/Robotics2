301e9f5920cea26fc1e7e11024bb28d970324586
rafaeljer <rafael.jer@gmail.com>
2023-01-20 12:36:25 +0000

    Added rotation matrix and produced yaml for path

--- guidance/find_path/main.py
+++ guidance/find_path/main.py
@@ -3,6 +3,7 @@
 import cv2 as cv
 import image as img
 import search as srch
+import numpy as np
 
 # advise to set PLOT to True in debug_plot.py to visualize results of the pipeline
 import debug_plot as dbg 
@@ -12,10 +13,10 @@ START_POINTS = [None, None, None, None] # must be SAME SIZE as IMAGES (277, 556)
 END_POINTS = [None, None, None, None] # must be SAME SIZE as IMAGES (719, 142)
 
 # list images available
-FILENAMES = ['images/tecnico.jpg', 'images/path1.jpg' , 'images/path2.png', 'images/path3.jpg']
+FILENAMES = ['images/tecnico.jpg', 'images/tecnico_1280.png' , 'images/path2.png', 'images/path3.jpg']
 
 # INDEXES OF IMAGES IN FILENAMES TO PROCESS
-TO_PROCESS = [0,1,2,3]
+TO_PROCESS = [1]
 
 FILENAMES = [FILENAMES[i] for i in TO_PROCESS]    
 START_POINTS = [START_POINTS[i] for i in TO_PROCESS]
@@ -46,10 +47,21 @@ MIN_DIST_CLUSTER = 2 # sort cluster window: minimum distance between 2 points of
 
 INTER_CLUSTER_DIST = 9 # maximum distance between 2 clusters to be considered neighbors
 
+
+########## CHANGE COORDINATES ##########
+
+# change from image frame to inertial (real world) frame
+# p_inertial = R_MATRIX * p_image
+# p is extended with one one, i.e. p = [x; y; 1]
+
+R_MATRIX = np.array([[-0.018623413997238, 0.231812495281406, -1.320596088340964e+02], 
+                     [-0.231812495281406, -0.018623413997238, 63.803816354536260],
+                     [0, 0, 1]])
+
 ########################### MAIN ###########################
 
 def main(start_points=None, end_points=None, filenames=None, default_values=False):
-    """ Can call this function with other scrips. If default_values is True, will use default 
+    """ Can call this function with other scripts. If default_values is True, will use default 
     values for start_points, end_points and filenames set in global variables FILENAME, START_POINTS and END_POINTS.
     Otherwise, will use the ones provided. start_points, end_points and filenames must be lists of the same size."""
 
@@ -117,13 +129,24 @@ def main(start_points=None, end_points=None, filenames=None, default_values=Fals
         points = srch.getPointsFromPath(path, map)
         dbg.plotPoints(map, points, start, end, filename, save_name=filename.split('.')[0]+'_path', gif = True)
 
-        ########## SAVE .TXT ##########
-            
-        # save x and y coordinates of clusters in txt file in main folder
-        with open(filename.split('.')[0] + '.txt', 'w') as f:
-            for x,y in points:
-                f.write(str(x) + ' ' + str(y) + '\n') 
+        ########## CHANGE COORDINATES ##########
         
+        # convert to numpy, add extra one element, and transpose
+        with open(filename.split('.')[0] + '.yaml', 'w') as f:
+            # convert points to numpy, add extra one element, and transpose
+            points_np = np.transpose(np.c_[np.array(points), np.ones(len(points))])
+
+            # multiply by transformation matrix R_MATRIX and remove extra element
+            real_points = np.transpose((R_MATRIX @ points_np)[:-1])
+
+            # write yaml file
+            string = 'reference_path: '+ str([[x,y] for x,y in real_points])
+            f.write(string.replace('],', '],\n'))
+
+            # write txt file
+            with open(filename.split('.')[0] + '.txt', 'w') as f:
+                for x,y in points:
+                    f.write(str(x) + ' ' + str(y) + '\n') 
              
 if __name__ == "__main__":
     main(default_values=True)
